64a7ee41087f4c8a4a007667ccfaa2e61b01dc68
Kevin Javier Muñante Alcedo <kevin.munante@utec.edu.pe>
2021-06-20 18:01:00 -0500

    OperdorM

---
+++ Semana02/OperadoresMat.py
@@ -0,0 +1,23 @@
+x = x + 5
+x = x - 10
+x = x/2
+x = x*4
+x = x + 20
+x = x // 2
+
+#Operadores de manipulacion de bits
+<< : x << y
+>> : x >> y
+&  : x & y
+|  : x | y
+~   : ~x
+
+#Funciones para manipulacion de bits
+bit_length() : (102).bit_length()
+oct()        : oct(79)
+bin()        : bin(10)
+hex()        : hex(400)
+int(base,numero) : int(0o117,8)
+
+#cadena
+"ob10101".replace("ob","")
