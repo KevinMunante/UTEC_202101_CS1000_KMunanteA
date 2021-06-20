ea96fcdce34a023f83dfa8a6de26db6a5aa353cd
Kevin Javier Muñante Alcedo <kevin.munante@utec.edu.pe>
2021-06-20 18:21:19 -0500

    PizarraS3

---
+++ Semana03/PisarraS3.py
@@ -0,0 +1,95 @@
+#Operadores basicos
+
+4+5 = 9
+7/2 = 3.5
+4-5 = -1
+4*5 = 20
+4**2 = 16
+
+#variable
+num = 10
+
+#reportes en pantalla
+print("Hola")
+print("{}={}.format(14,¨1110"))  14 = 1110
+
+#Proposiciones logicos
+
+A or B : Tu bebes a las 9 am
+A and B : Tú bebes café mezclaado con leche
+
+#manipulacion de bits
+
+x = 14
+x>>=3
+print(x)
+
+print(12>>1)
+6
+
+print(12<<2)
+48
+
+print(23<<3)
+184
+
+#Operador Or Bits
+
+print(6|8)
+14
+
+x = 19
+x|=17
+print(x)
+
+#Operador AND bits
+print(6&21)
+4
+
+Pro:
+x = 12
+x&=7
+print(x)
+4
+
+#Sistema Decimal
+num = "10"
+num = int(num)
+
+#De Binario a Decimal
+num = "ob110"
+num = int(num,2)
+
+#De Octal a  Decimal
+num = "Oo10"
+decimal = int(num,8)
+
+#De Hexadecimal a Decimal
+num = "0x12
+decimal = int(num, 16)
+
+#Operadores relacionales
+
+1. 19+20 < 89+7
+      39 < 96
+        True
+
+2. 4|3 < 3|3
+     7 < 3
+     False
+
+3. 7&5 == 5 | 0
+
+Slices:
+
+c = "Hola Mundo"
+print(c)
+
+print(c[1,3])
+[1,3> = {1,2}
+
+print(c [5:])
+[5,+oo] = {5,6,7,8,9}
+
+print(c[:3])
+[-00, 3] = {0,1,2}
